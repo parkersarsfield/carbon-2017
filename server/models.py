6f24c3ee48e0c54b6f716ad4f6e14e71f92dcecb
Kenneth Schnall <kas6570@psu.edu>
2017-06-07 19:57:23 -0400

    Add password to create.py utility module

--- server/models.py
+++ server/models.py
@@ -2,7 +2,7 @@ from config import db_host, db_name, db_password, db_user
 from peewee import *
 import datetime
 
-db = PostgresqlDatabase(db_name, host=db_host, user=db_user, password=db_password)
+db = PostgresqlDatabase(db_name, host=db_host, password=db_password, user=db_user)
 
 class BaseModel(Model):
     class Meta:
--- server/util/db/create.py
+++ server/util/db/create.py
@@ -2,11 +2,11 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
-from config import db_host, db_name, db_user
+from config import db_host, db_name, db_password, db_user
 from models import Transaction
 from peewee import *
 
-db = PostgresqlDatabase(db_name, host=db_host, user=db_user)
+db = PostgresqlDatabase(db_name, host=db_host, password=db_password, user=db_user)
 
 db.connect()
 
