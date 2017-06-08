b4304df305277eeb258228bdbd7af5fa3a34884d
Kenneth Schnall <kas6570@psu.edu>
2017-06-08 09:46:08 -0400

    Move global variables to inside class functions

--- myo/app.py
+++ myo/app.py
@@ -5,10 +5,11 @@ import requests
 
 api_url = 'https://warm-spire-75113.herokuapp.com/api/validate'
 
+current_gesture = 0
 gesture = None
 gestures_map = {libmyo.Pose.fist:'FIST', libmyo.Pose.wave_in:'LEFT', libmyo.Pose.fingers_spread:'OPEN', libmyo.Pose.wave_out:'RIGHT'}
 
-def check_gesture(current_gesture):
+def check_gesture():
     print('current_gesture:', current_gesture)
 
     data = {'current_gesture':current_gesture, 'gesture':gesture, 'transaction_id':0}
@@ -21,9 +22,6 @@ def check_gesture(current_gesture):
     print(result)
 
 class Listener(libmyo.DeviceListener):
-    def __init__(self):
-        self.current_gesture = 0
-
     def on_pair(self, myo, timestamp, firmware_version):
         print("Hello, Myo!")
 
@@ -31,16 +29,19 @@ class Listener(libmyo.DeviceListener):
         print("Goodbye, Myo!")
 
     def on_pose(self, myo, timestamp, pose):
+        global current_gesture
+        global gesture
+
         if not (pose == libmyo.Pose.rest) and not (pose == libmyo.Pose.double_tap):
-            if self.current_gesture >= 3:
-                self.current_gesture = 0
+            if current_gesture >= 3:
+                current_gesture = 0
 
             gesture = gestures_map[pose]
-            self.current_gesture = self.current_gesture + 1
+            current_gesture = current_gesture + 1
 
             myo.vibrate('short')
 
-            check_gesture(self.current_gesture)
+            check_gesture()
 
 hub = libmyo.Hub()
 hub.run(1000, Listener())
